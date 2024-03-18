040ad2bee0ab28ab2cfbe445f8cf6f8c653ae6bc
saptarshi-2023-14 <saptarshi.adhya.37@gmail.com>
2024-03-18 22:58:00 +0530

    my 16th code

---
+++ codechef/course/LBCL08/problems/CLB038.c
@@ -0,0 +1,13 @@
+// Task
+// Write a program which does the following
+
+// Create a string named fruit and assign the value "Apple" to it.
+// Output fruit to the console.
+#include <stdio.h>
+
+int main() {
+
+    char fruit[] = "Apple";
+    printf("%s", fruit);
+return 0;
+}
